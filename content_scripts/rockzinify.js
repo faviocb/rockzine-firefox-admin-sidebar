661a9ced80547a98c4707db869c00fb77a10ff76
faviocb <usuarioasp@hotmail.com>
2018-04-25 22:39:39 +1000

    refactoring

--- content_scripts/rockzinify.js
+++ content_scripts/rockzinify.js
@@ -1,168 +1,121 @@
-(function() {
+(function () {
 
-	  
-  /**
-   * Check and set a global guard variable.
-   * If this content script is injected into the same page again,
-   * it will do nothing next time.
-   */
-  if (window.hasRun) {
-    return;
-  }
-  
-  window.hasRun = true;
-  
-  
- 
-  console.info('__________ Rockzine :) 		$$$$$$$$$$ rockzinifying now $$$$$$$$$$');
+	/**
+	 * Check and set a global guard variable.
+	 * If this content script is injected into the same page again,
+	 * it will do nothing next time.
+	 */
+	if (window.hasRun) {
+		return;
+	}
+	window.hasRun = true;
 
-  
-  
-	const PEOPLE_WHO_LIKE_THIS_PAGE = "PEOPLE_WHO_LIKE_THIS_PAGE";
-	const PEOPLE_WHO_FOLLOW_THIS_PAGE = "PEOPLE_WHO_FOLLOW_THIS_PAGE";
-	const PAGES_THAT_LIKE_THIS_PAGE = "PAGES_THAT_LIKE_THIS_PAGE";
-	const PEOPLE_BANNED_FROM_PAGE = "PEOPLE_BANNED_FROM_PAGE";
+	console.info('__________ Rockzine :) 		$$$$$$$$$$ rockzinifying now $$$$$$$$$$');
 
-
-
-	 
 
 	//==============================================================================================================================
 	//===============================================		Helpers		============================================================
 	//==============================================================================================================================
 
-	function sendData(data, url ) {
+	function sendData(data, url) {
 
-		return new Promise(function(resolve, reject){
+		return new Promise(function (resolve, reject) {
 
 			var XHR = new XMLHttpRequest();
 			var urlEncodedData = "";
 			var urlEncodedDataPairs = [];
 			var name;
-	  
+
 			// Turn the data object into an array of URL-encoded key/value pairs.
-			for(name in data) {
-			  urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+			for (name in data) {
+				urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
 			}
-	  
+
 			// Combine the pairs into a single string and replace all %-encoded spaces to 
 			// the '+' character; matches the behaviour of browser form submissions.
 			urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
-	  
+
 			// Define what happens on successful data submission
-			XHR.addEventListener('load', function(event) {
+			XHR.addEventListener('load', function (event) {
 
 				if (this.status == 200) {
 					resolve(this.response);					// Success :)
-				} 
+				}
 				else {
 					reject({
-					  errorStatus: this.status,
-					  errorStatusText: this.statusText,
-					  errorText :this.responseText,
-					  targetUrl: url
+						errorStatus: this.status,
+						errorStatusText: this.statusText,
+						errorText: this.responseText,
+						targetUrl: url
 					});										// Failure returned by Server :(
 				}
 			});
-	  
+
 			// Define what happens in case of error
-			XHR.addEventListener('error', function(event) {
+			XHR.addEventListener('error', function (event) {
 
 				reject({
 					errorStatus: -1,
-					errorStatusText: '',					
+					errorStatusText: '',
 					errorText: 'An error ocurred when trying to send data to server',
 					targetUrl: url
 				});											// Failure, God knows why :(
 			});
-	  
+
 			// Set up our request
 			XHR.open('POST', url);
-	  
+
 			// Add the required HTTP header for form data POST requests
 			XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	  
+
 			// Finally, send our data.
 			XHR.send(urlEncodedData);
-	  
+
 
 		});
 
 	}
 
-
 	//==============================================================================================================================
 	//==============================================================================================================================
 
-	 
-	 
-	 
-  browser.runtime.onMessage.addListener((message) => {
-	  
-		return new Promise(function(resolve, reject){
 
+	browser.runtime.onMessage.addListener((message) => {
 
-			switch(message.requestType) {
-				case PEOPLE_WHO_LIKE_THIS_PAGE:
-				
-					
-					var url =  message.facebookUrl  + message.requestType + '&page_id=' + message.targetPageId + '&offset=0&limit=100&dpr=2';
-	
-					var data = 	{
-								__user: message.__user,
-								__a: message.__a,
-								__dyn: message.__dyn,
-								__req: message.__req,
-								__be: message.__be,
-								__pc: message.__pc,
-								__rev: message.__rev,
-								fb_dtsg: message.fb_dtsg,
-								jazoest: message.jazoest,
-								__spin_r: message.__spin_r,
-								__spin_b: message.__spin_b,
-								__spin_t: message.__spin_t
-								}
-								
-					var facebookRequestPromise = sendData(data, url);
+		return new Promise(function (resolve, reject) {
 
-					facebookRequestPromise.then(function (response){
-										console.info('__________ Rockzine :) 	datos obtenidos de facebook con exito.');
-										resolve(response)
-	
-									})
-							.catch(function(err){
-										console.info('__________ Rockzine :( 	error al tratar de obtener datos de facebook');
-										reject(new Error(JSON.stringify(err)))
-									});
-					
-					break;
-				case PEOPLE_WHO_FOLLOW_THIS_PAGE:
-					alert('followers')
-					break;
-				case PAGES_THAT_LIKE_THIS_PAGE:
-					alert('pages like')
-					break;
-				case PEOPLE_BANNED_FROM_PAGE:
-					alert('banneados')
-					break;
-		
-			}			
+			var url = message.facebookUrl + message.requestType + '&page_id=' + message.targetPageId + '&offset=0&limit=100&dpr=2';
 
+			var data = {
+				__user: message.__user,
+				__a: message.__a,
+				__dyn: message.__dyn,
+				__req: message.__req,
+				__be: message.__be,
+				__pc: message.__pc,
+				__rev: message.__rev,
+				fb_dtsg: message.fb_dtsg,
+				jazoest: message.jazoest,
+				__spin_r: message.__spin_r,
+				__spin_b: message.__spin_b,
+				__spin_t: message.__spin_t
+			}
+
+			var facebookRequestPromise = sendData(data, url);
+
+			facebookRequestPromise
+				.then(function (response) {
+					console.info('__________ Rockzine :) 	datos obtenidos de facebook con exito.');
+					resolve(response)
+
+				})
+				.catch(function (err) {
+					console.info('__________ Rockzine :( 	error al tratar de obtener datos de facebook');
+					reject(new Error(JSON.stringify(err)))
+				});
 
 		});
 
-		
-	  
+	});
 
-   
-  });	 
-	 
-	 
-	 
-	 
-	 
-	 
-
-	 
-	 
 })();
--- sidebar/rockzine-admin.js
+++ sidebar/rockzine-admin.js
@@ -7,150 +7,98 @@ const PEOPLE_BANNED_FROM_PAGE = "PEOPLE_BANNED_FROM_PAGE";
 
 const ZONAJOVEN_PAGE_ID = 165113237807;
 
-var flagPEOPLE_WHO_LIKE_THIS_PAGE = false;
-var flagPEOPLE_WHO_FOLLOW_THIS_PAGE = false;
-var flagPAGES_THAT_LIKE_THIS_PAGE = false;
-var flagPEOPLE_BANNED_FROM_PAGE = false;
+var flagFacebookParametersCaptured = false;
 
 
+//==================================================================================================
 
+function reportErrorBeforeImport(error) {
+	console.info('98) __________ Rockzine :(		error before connecting to Facebook page');
+	console.info(error);
+}
 
+function reportImportError(error) {
+	console.info('97) __________ Rockzine :(		error getting data from Facebook');
+	console.info(error);
+}
 
-    function reportErrorBeforeExport(error) {
-	  console.info('98) __________ Rockzine :(		error before connecting to Facebook page');
-	  console.info(error);
-    }
+function reportImportSuccess(result) {
+	console.info('04) __________ Rockzine :)		Success getting data from Facebook');
+	console.info(result);
+}
 
-    function reportExportError(error) {
-		console.info('97) __________ Rockzine :(		error getting data from Facebook');
-		console.info(error);
+//==================================================================================================
+
+function ImportAndSave(usersPool, formData){
+
+	browser.tabs.query({ active: true, currentWindow: true })
+	.then(
+		(tabs) => {
+			browser.tabs.sendMessage(tabs[0].id, {
+				requestType: usersPool,
+				targetPageId: ZONAJOVEN_PAGE_ID,
+				facebookUrl: FACEBOOK_URL,
+
+				__a: formData.__a[0],
+				__be: formData.__be[0],
+				__dyn: formData.__dyn[0],
+				__pc: formData.__pc[0],
+				__req: formData.__req[0],
+				__rev: formData.__rev[0],
+				__spin_b: formData.__spin_b[0],
+				__spin_r: formData.__spin_r[0],
+				__spin_t: formData.__spin_t[0],
+				__user: formData.__user[0],
+				fb_dtsg: formData.fb_dtsg[0],
+				jazoest: formData.jazoest[0]
+
+			})
+				.then(reportImportSuccess)
+				.catch(reportImportError);
+		}
+	)
+	.catch(reportErrorBeforeImport);
+}
+
+//==================================================================================================
+
+function captureFacebookParameters(e) {
+
+	if (!flagFacebookParametersCaptured) 
+	{
+		if (	e.url.includes(FACEBOOK_URL + PEOPLE_WHO_LIKE_THIS_PAGE) ||
+				e.url.includes(FACEBOOK_URL + PEOPLE_WHO_FOLLOW_THIS_PAGE) ||
+				e.url.includes(FACEBOOK_URL + PAGES_THAT_LIKE_THIS_PAGE) ||
+				e.url.includes(FACEBOOK_URL + PEOPLE_BANNED_FROM_PAGE)
+			) 
+		{
+
+			flagFacebookParametersCaptured = true;
+
+			console.info('03) __________ Rockzine :)	Facebook parameters captured!');
+
+			var formData = e.requestBody.formData;
+
+			ImportAndSave(PEOPLE_WHO_LIKE_THIS_PAGE, formData);
+			// ImportAndSave(PEOPLE_WHO_FOLLOW_THIS_PAGE, formData);
+			// ImportAndSave(PAGES_THAT_LIKE_THIS_PAGE, formData);
+			// ImportAndSave(PEOPLE_BANNED_FROM_PAGE, formData);
+
+		}
 	}
 
-	function reportExportSuccess(result) {
-		console.info('04) __________ Rockzine :)		Success getting data from Facebook');
-		console.info(result);
-	}
-
-	function captureFacebookParameters(e) {
-	
-
-		if(!flagPEOPLE_WHO_LIKE_THIS_PAGE){
-			if(e.url.includes(FACEBOOK_URL + PEOPLE_WHO_LIKE_THIS_PAGE )){
-				flagPEOPLE_WHO_LIKE_THIS_PAGE = true;
-
-				console.info('03) __________ Rockzine :)	captured =====> ' + PEOPLE_WHO_LIKE_THIS_PAGE) ;
-				
-				var formData = e.requestBody.formData;
-				
-				browser.tabs.query({active: true, currentWindow: true})
-						  .then(
-							  (tabs) => {
-								          browser.tabs.sendMessage(tabs[0].id, {
-											  requestType: PEOPLE_WHO_LIKE_THIS_PAGE,
-											  targetPageId :ZONAJOVEN_PAGE_ID,
-											  facebookUrl: FACEBOOK_URL,
-											  
-											  __a : formData.__a[0],
-											  __be : formData.__be[0],
-											  __dyn : formData.__dyn[0],
-											  __pc : formData.__pc[0],
-											  __req : formData.__req[0],
-											  __rev : formData.__rev[0],
-											  __spin_b : formData.__spin_b[0],
-											  __spin_r : formData.__spin_r[0],
-											  __spin_t : formData.__spin_t[0],
-											  __user : formData.__user[0],
-											  fb_dtsg : formData.fb_dtsg[0],
-											  jazoest : formData.jazoest[0]											  
-											  
-											})
-											.then(reportExportSuccess)
-											.catch(reportExportError);
-								}
-						  )
-						.catch(reportErrorBeforeExport);				
-				
-				
-			}
-		}
-		
-		
-
-		if(!flagPEOPLE_WHO_FOLLOW_THIS_PAGE){
-			if(e.url.includes(FACEBOOK_URL + PEOPLE_WHO_FOLLOW_THIS_PAGE )){
-				flagPEOPLE_WHO_FOLLOW_THIS_PAGE = true;
-				console.info('=====> ' + PEOPLE_WHO_FOLLOW_THIS_PAGE) ;
-				//Export(PEOPLE_WHO_FOLLOW_THIS_PAGE);
-				
-				browser.tabs.query({active: true, currentWindow: true})
-						  .then(
-							  (tabs) => {
-								          browser.tabs.sendMessage(tabs[0].id, {
-											  requestType: PEOPLE_WHO_FOLLOW_THIS_PAGE,
-											});
-								}
-						  )
-						.catch(reportErrorBeforeExport);								
-
-						
-			}
-		}
-
-
-		if(!flagPAGES_THAT_LIKE_THIS_PAGE){
-			if(e.url.includes(FACEBOOK_URL + PAGES_THAT_LIKE_THIS_PAGE )){
-				flagPAGES_THAT_LIKE_THIS_PAGE = true;
-				console.info('=====> ' + PAGES_THAT_LIKE_THIS_PAGE) ;
-				//Export(PAGES_THAT_LIKE_THIS_PAGE);
-				
-				browser.tabs.query({active: true, currentWindow: true})
-						  .then(
-							  (tabs) => {
-								          browser.tabs.sendMessage(tabs[0].id, {
-											  requestType: PAGES_THAT_LIKE_THIS_PAGE,
-											});
-								}
-						  )
-						.catch(reportErrorBeforeExport);							
-						
-			}
-		}
-
-
-		if(!flagPEOPLE_BANNED_FROM_PAGE){
-			if(e.url.includes(FACEBOOK_URL + PEOPLE_BANNED_FROM_PAGE )){
-				flagPEOPLE_BANNED_FROM_PAGE = true;
-				console.info('=====> ' + PEOPLE_BANNED_FROM_PAGE) ;
-				//Export(PEOPLE_BANNED_FROM_PAGE);
-				
-				browser.tabs.query({active: true, currentWindow: true})
-						  .then(
-							  (tabs) => {
-								          browser.tabs.sendMessage(tabs[0].id, {
-											  requestType: PEOPLE_BANNED_FROM_PAGE,
-											});
-								}
-						  )
-						.catch(reportErrorBeforeExport);							
-						
-			}
-		}
-		
-		
-	}
+}
 
 function listenForEvents() {
-	
-	console.info('02) __________ Rockzine :)	Listening');
-	
-	
-	 browser.webRequest.onBeforeRequest.addListener(
-	  captureFacebookParameters,
-	  {urls: ["<all_urls>"]},
-	  ["blocking", "requestBody"]
-	);	
 
-	//================================================================
+	console.info('02) __________ Rockzine :)	Listening');
+
+	browser.webRequest.onBeforeRequest.addListener(
+		captureFacebookParameters,
+		{ urls: ["<all_urls>"] },
+		["blocking", "requestBody"]
+	);
+
 }
 
 
@@ -159,18 +107,18 @@ function listenForEvents() {
 
 
 function reportExecuteScriptError(error) {
-	
-  document.querySelector("#sidebar-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");	
-	
-  console.info(`99) __________ Rockzine :( 		Error al rockzinify: ${error.message}`);
+
+	document.querySelector("#sidebar-content").classList.add("hidden");
+	document.querySelector("#error-content").classList.remove("hidden");
+
+	console.info(`99) __________ Rockzine :( 		Error al rockzinify: ${error.message}`);
 }
 
 
 
 console.info('01) __________ Rockzine :)	Begin');
 
- 
-browser.tabs.executeScript({file: "/content_scripts/rockzinify.js"})
- .then(listenForEvents, reportExecuteScriptError)
+
+browser.tabs.executeScript({ file: "/content_scripts/rockzinify.js" })
+	.then(listenForEvents, reportExecuteScriptError)
 
